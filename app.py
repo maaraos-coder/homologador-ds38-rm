a8d3bcff71cbdf021bfc66d9941c54ba8d46aac0
maaraos-coder <maaraos@gmail.com>
2026-06-04 17:55:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1124,8 +1124,7 @@ st.success(categorias)
     # =====================================================
 
 st.markdown("## 4. Clasificación D.S. N°38/2011 MMA")
-
-    col_zona, col_dia, col_noche = st.columns([2, 1, 1])
+col_zona, col_dia, col_noche = st.columns([2, 1, 1])
 
     with col_zona:
         st.markdown(
