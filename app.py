c74f2ab2f7dbf705f2c23215201cde7cc1dc948e
maaraos-coder <maaraos@gmail.com>
2026-06-10 16:03:41 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1105,6 +1105,13 @@ def mostrar_resultado(lat, lon, gdf_prc, gdf_prms_uso, gdf_prms_lu, tolerancia_m
 
     fila = resultado.iloc[0]
 
+
+st.markdown("### DEBUG PRMS")
+
+for campo in fila.index:
+    st.write(campo, "=", fila[campo])
+
+    
     zona_ds38, limite_dia, limite_noche, criterio, categorias = homologar_ds38(
         fila,
         estado_lu
